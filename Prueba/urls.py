18dc18457903e1f46bb36de299371831627b9792
juanmasorca <juanmasorca@gmail.com>
2023-04-06 15:13:04 -0500

    update

--- Prueba/urls.py
+++ Prueba/urls.py
@@ -4,9 +4,25 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('empleados/', views.empleados, name='empleados'),
+
+    path('agregar_empleado/', views.agregar_empleado, name='agregar empleado'),
+
+    path('eliminar_empleado/', views.vista_eliminar_empleado,
+         name='vista eliminar empleado'),
+
+    path('eliminar_empleado/<int:codigo>/',
+         views.eliminar_empleado, name='eliminar empleado'),
+
+    path('departamentos/', views.departamentos, name='departamentos'),
+
     path('agregar_departamento/', views.agregar_departamento,
          name='agregar departamento'),
-    path('departamentos/', views.departamentos,
-         name='departamentos'),
+
+    path('eliminar_departamento/', views.vista_eliminar_departamento,
+         name='vista eliminar departamento'),
+
+    path('eliminar_departamento/<int:codigo>/',
+         views.eliminar_departamento, name='eliminar departamento'),
 ]
--- Prueba/views.py
+++ Prueba/views.py
@@ -1,19 +1,11 @@
 from django.shortcuts import render, redirect
-from .models import departamento
+from .models import departamento, Empleado
 
 # Create your views here.
 
 
 def index(request):
     return render(request, 'index.html')
-
-
-def empleados(request):
-    return render(request, 'empleados/empleados.html')
-
-
-def agregar_empleado(request):
-    return redirect(request, )
 
 
 def departamentos(request):
@@ -32,3 +24,54 @@ def agregar_departamento(request):
         nuevo_departamento.save()
         # print(request.POST)
         return redirect('agregar departamento')
+
+
+def vista_eliminar_departamento(request):
+    if request.method == 'GET':
+        lista_departamentos = departamento.objects.all()
+        # sow interface
+        return render(request, 'departamento/eliminar_departamento.html', {'departamentos': lista_departamentos})
+    else:
+        return redirect('vista eliminar departamento')
+
+
+def eliminar_departamento(request, codigo):
+    departamento_a_eliminar = departamento.objects.get(codigo=codigo)
+    print(departamento_a_eliminar.nombre)
+    departamento_a_eliminar.delete()
+    return redirect('vista eliminar departamento')
+
+
+def empleados(request):
+    lista_empleados = Empleado.objects.all()
+
+    return render(request, 'empleados/empleados.html', {'empleados': lista_empleados})
+
+
+def agregar_empleado(request):
+    if request.method == 'GET':
+        # sow interface
+        lista_departamentos = departamento.objects.all()
+        return render(request, 'empleados/agregar_empleado.html', {'departamentos': lista_departamentos})
+    else:
+        nuevo_empleado = Empleado(codigo=request.POST['codigo'], nif=request.POST['nif'], nombre=request.POST['nombre'],
+                                  apellido1=request.POST['apellido1'], apellido2=request.POST['apellido2'], codigo_departamento_id=request.POST['departamento'])
+        nuevo_empleado.save()
+        print(request.POST)
+        return redirect('agregar empleado')
+
+
+def vista_eliminar_empleado(request):
+    if request.method == 'GET':
+        lista_empleados = Empleado.objects.all()
+        # sow interface
+        return render(request, 'empleados/eliminar_empleados.html', {'empleados': lista_empleados})
+    else:
+        return redirect('vista eliminar empleado')
+
+
+def eliminar_empleado(request, codigo):
+    empleado_a_eliminar = Empleado.objects.get(codigo=codigo)
+    print(empleado_a_eliminar.nombre)
+    empleado_a_eliminar.delete()
+    return redirect('vista eliminar empleado')
